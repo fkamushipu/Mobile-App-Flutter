eabc1e94d5b39babf54c93b96cdad26ce596d487
Feni Kamushipu <fkamushipu@gmail,com>
2022-12-09 09:03:24 +0200

    File addition

---
+++ mobile_template/lib/Splash.dart
@@ -0,0 +1,9 @@
+// Align all enclosed element in the center
+const import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Center (
+  child: Text(
+    style: TextStyle(fontSize: 20),
+  ),
+)
